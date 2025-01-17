77485dc69cda07590a5592be974c455b504cade6
snehavarshney <snehavarshney111@gmail.com>
2025-01-17 13:46:33 +0530

    Added: subtraction of 3 numbers

---
+++ src/subtraction.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class subtraction {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter value of 1st number ::");
+        int num1 = sc.nextInt();
+
+        System.out.println("Enter value of 2nd number ::");
+        int num2 = sc.nextInt();
+
+        System.out.println("Enter value of 3rd number ::");
+        int num3 = sc.nextInt();
+
+        System.out.println("Sum of the given two numbers: "+(num1-num2-num3));
+    }
+}
