522f9c564d972b595cf59f69661a86371ef1295c
snehavarshney <snehavarshney111@gmail.com>
2025-01-18 14:25:26 +0530

    Add helloworld.java

---
+++ Desktop/GLA/helloworld.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class helloworld
+{
+public static void main(String [] args)
+{
+int j=10;
+for(int i=1;i<=j;i++)
+{
+if (i%2!=0)
+{
+System.out.print(i);
+System.out.print(" ");
+}
+}
+}
+}
